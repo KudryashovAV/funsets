516a073483a1f9a4f21eea39a8eefb2ce89232dd
Alexey Kudryashov <akd@krononsoft.com>
2023-09-07 11:46:50 +0300

    Solution

--- src/test/scala/funsets/FunSetSuite.scala
+++ src/test/scala/funsets/FunSetSuite.scala
@@ -13,6 +13,10 @@ class FunSetSuite extends munit.FunSuite:
     assert(contains(x => true, 100))
   }
 
+  test("it does not contain 100") {
+    assert(!contains({(x: Int) => x > 0}, -100))
+  }
+
   /**
    * When writing tests, one would often like to re-use certain values for multiple
    * tests. For instance, we would like to create an Int-set and have multiple test
@@ -32,42 +36,89 @@ class FunSetSuite extends munit.FunSuite:
    *
    */
 
-  trait TestSets:
-    val s1 = singletonSet(1)
-    val s2 = singletonSet(2)
-    val s3 = singletonSet(3)
+  trait TestSets {
+    val s1: _root_.funsets.FunSets.FunSet = singletonSet(1)
+    val s2: _root_.funsets.FunSets.FunSet = singletonSet(2)
+    val s3: _root_.funsets.FunSets.FunSet = singletonSet(3)
+    val s4: _root_.funsets.FunSets.FunSet = singletonSet(1)
+    val s5: _root_.funsets.FunSets.FunSet = singletonSet(4)
+    val positiveNumbersSet: _root_.funsets.FunSets.FunSet = union(singletonSet(1), singletonSet(300))
+    val negativeNumbersSet: _root_.funsets.FunSets.FunSet = union(singletonSet(-10), singletonSet(-99))
+    val setPositiveAndNegativeNumbers: _root_.funsets.FunSets.FunSet = union(positiveNumbersSet, negativeNumbersSet)
+    val evenNumbersSet: _root_.funsets.FunSets.FunSet = union(singletonSet(4), singletonSet(6))
+    val oddNumbersSet: _root_.funsets.FunSets.FunSet = union(singletonSet(3), singletonSet(9))
+    val evenAndOddNumbersSet: _root_.funsets.FunSets.FunSet = union(evenNumbersSet, oddNumbersSet)
+  }
 
-  /**
-   * This test is currently disabled (by using .ignore) because the method
-   * "singletonSet" is not yet implemented and the test would fail.
-   *
-   * Once you finish your implementation of "singletonSet", remove the
-   * .ignore annotation.
-   */
-
-    test("singleton set one contains one".ignore) {
-    
-    /**
-     * We create a new instance of the "TestSets" trait, this gives us access
-     * to the values "s1" to "s3".
-     */
+  test("SingletonSet function. SingletonSet(1) contains one") {
     new TestSets:
-      /**
-       * The string argument of "assert" is a message that is printed in case
-       * the test fails. This helps identifying which assertion failed.
-       */
       assert(contains(s1, 1), "Singleton")
   }
 
-  test("union contains all elements of each set") {
+  test("SingletonSet function. SingletonSet(1) contains two") {
     new TestSets:
-      val s = union(s1, s2)
+      assert(!contains(s1, 2), "Singleton does not contain 2")
+  }
+
+  test("Union function") {
+    new TestSets:
+      val s: _root_.funsets.FunSets.FunSet = union(s1, s2)
       assert(contains(s, 1), "Union 1")
       assert(contains(s, 2), "Union 2")
       assert(!contains(s, 3), "Union 3")
   }
 
+  test("Intersect function") {
+    new TestSets:
+      val intersection1: _root_.funsets.FunSets.FunSet = intersect(s1, s2)
+      assert(!contains(intersection1, 1), "Intersect 1 between singletonSet(1) and singletonSet(2)")
+      val intersection2: _root_.funsets.FunSets.FunSet = intersect(s1, s4)
+      assert(contains(intersection2, 1), "Intersect 1 between singletonSet(1) and singletonSet(4)")
+      assert(!contains(intersection2, 2), "Intersect 2 between singletonSet(1) and singletonSet(4)")
+  }
 
+  test("Diff function") {
+    new TestSets:
+      val difference1: _root_.funsets.FunSets.FunSet = diff(s1, s2)
+      assert(contains(difference1, 1), "Diff 1 between singletonSet(1) and singletonSet(2)")
+      val difference2: _root_.funsets.FunSets.FunSet = diff(s1, s4)
+      assert(!contains(difference2, 1), "Diff 2 between singletonSet(1) and singletonSet(4)")
+  }
+
+  test("Filter function") {
+    new TestSets:
+      val filterSet1: _root_.funsets.FunSets.FunSet = filter(s1, {(elem: Int) => elem < 2})
+      assert(contains(filterSet1, 1))
+
+      val filterSet2: _root_.funsets.FunSets.FunSet = filter(s3, {(elem: Int) => elem > 5})
+      assert(!contains(filterSet2, 3))
+  }
+
+  test("Forall function") {
+    new TestSets:
+      assert(forall(positiveNumbersSet, {(elem: Int) => elem > 0}))
+      assert(forall(negativeNumbersSet, {(elem: Int) => elem < 0}))
+      assert(!forall(setPositiveAndNegativeNumbers, {(elem: Int) => elem < 0}))
+      assert(forall(evenNumbersSet, {(elem: Int) => (elem % 2) == 0}))
+      assert(forall(oddNumbersSet, {(elem: Int) => (elem % 2) != 0}))
+      assert(!forall(evenAndOddNumbersSet, {(elem: Int) => (elem % 2) == 0}))
+  }
+
+  test("Exists function") {
+    new TestSets:
+      assert(exists(setPositiveAndNegativeNumbers, {(elem: Int) => elem > 0}))
+      assert(exists(evenAndOddNumbersSet, {(elem: Int) => (elem % 2) == 0}))
+  }
+
+  test("Map function") {
+    new TestSets:
+      val mapEvenSetToOdd: _root_.funsets.FunSets.FunSet = map(evenNumbersSet, { (elem: Int) => elem + 1})
+      assert(contains(mapEvenSetToOdd, 5) && contains(mapEvenSetToOdd, 7))
+
+      val mapOddSetToEven: _root_.funsets.FunSets.FunSet = map(oddNumbersSet, { (elem: Int) => elem * 2})
+      assert(forall(mapOddSetToEven, {(elem: Int) => (elem % 2) == 0}))
+  }
 
   import scala.concurrent.duration.*
   override val munitTimeout = 10.seconds
+  
