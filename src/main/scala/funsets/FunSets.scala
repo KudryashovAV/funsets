be851f3ce2f0395c8eb6a9b0c78f002a556e00e8
Alexey Kudryashov <akd@krononsoft.com>
2023-09-07 16:12:36 +0300

    Cleanup and refactoring

--- src/main/scala/funsets/FunSets.scala
+++ src/main/scala/funsets/FunSets.scala
@@ -20,31 +20,31 @@ trait FunSets extends FunSetsInterface:
   /**
    * Returns the set of the one given element.
    */
-  def singletonSet(elem: Int): FunSet = {(newElem: Int) => elem == newElem}
+  def singletonSet(elem: Int): FunSet = x => elem == x
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-  def union(s: FunSet, t: FunSet): FunSet = {(newElem: Int) => contains(s,newElem) || contains(t, newElem)}
+  def union(s: FunSet, t: FunSet): FunSet = x => contains(s, x) || contains(t, x)
 
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-  def intersect(s: FunSet, t: FunSet): FunSet = {(newElem: Int) => contains(s,newElem) && contains(t, newElem)}
+  def intersect(s: FunSet, t: FunSet): FunSet = x => contains(s, x) && contains(t,x)
 
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-  def diff(s: FunSet, t: FunSet): FunSet = {(newElem: Int) => contains(s,newElem) && !contains(t, newElem)}
+  def diff(s: FunSet, t: FunSet): FunSet = x => contains(s, x) && !contains(t, x)
 
   /**
    * Returns the subset of `s` for which `p` holds.
    */
 
-  def filter(s: FunSet, p: Int => Boolean): FunSet = {(newElem: Int) => contains(s,newElem) && contains(p,newElem)}
+  def filter(s: FunSet, p: Int => Boolean): FunSet = x => contains(s, x) && p(x)
 
   /**
    * The bounds for `forall` and `exists` are +/- 1000.
@@ -54,49 +54,29 @@ trait FunSets extends FunSetsInterface:
   /**
    * Returns whether all bounded integers within `s` satisfy `p`.
    */
-  def forall(s: FunSet, p: Int => Boolean): Boolean = {
+  def forall(s: FunSet, p: Int => Boolean): Boolean =
     @tailrec
-    def iter(a: Int): Boolean = {
-      if (a == -bound) {
+    def iter(a: Int): Boolean =
+      if (a == -bound)
         true
-      }
-      else if (contains(s, a) && (!contains(filter(s, p), a))) {
+      else if (contains(s, a) && (!contains(filter(s, p), a)))
         false
-      }
-      else {
+      else
         iter(a - 1)
-      }
-    }
 
     iter(bound)
-  }
 
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
 
-  def exists(s: FunSet, p: Int => Boolean): Boolean = {
-    @tailrec
-    def iter(a: Int): Boolean = {
-      if (a == -bound) {
-        false
-      }
-      else if (contains(s, a) && contains(filter(s, p), a)) {
-        true
-      }
-      else {
-        iter(a - 1)
-      }
-    }
-
-    iter(bound)
-  }
-
+  def exists(s: FunSet, p: Int => Boolean): Boolean = !forall(s, x => !p(x))
+    
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-  def map(s: FunSet, f: Int => Int): FunSet = {(elem: Int) => exists(s, {(elem2: Int) => f(elem2) == elem})}
+  def map(s: FunSet, f: Int => Int): FunSet = y => exists(s, x => f(x) == y)
 
   /**
    * Displays the contents of a set
